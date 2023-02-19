a00cf06ace493fb1bd6e9b7ac1b1da6e5c98c5ea
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2023-02-19 13:26:22 +0530

    add initial java files

---
+++ LetsUpgradeJava/Java/day4.java
@@ -0,0 +1,11 @@
+//write a program to find the sum of 5 integers
+public class day4 {
+    public static void main(String[] args) {
+        int sum =0;
+        for(int i=1;i<=5;i++){
+            sum = sum+i;
+        }
+        System.out.println("the sum of 5 integers : " + sum);
+    }
+    
+}
